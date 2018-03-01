570a89c41892d3dd1428d67175f7092a408489a6
Ryan Price <ryapric@gmail.com>
2018-02-28 18:49:09 -0600

    Change `guess_txt` to not read in whole file first.
    Though I'm sure there's certainly a more elegant way to handle what I wrote, I can't get `...` to pass to the wrapped function, so.

--- R/readit.R
+++ R/readit.R
@@ -1,3 +1,68 @@
+#' Read Files of Any Type
+#'
+#' Given a file path, read the data into R, regardless of file type/extension.
+#' `readit` is a thick wrapper around many of the
+#' [tidyverse](https://www.tidyverse.org/) libraries, but can be forced to use
+#' base functions where possible. Note that the caveat is that the file
+#' _**needs**_ to have an extension, as well as be of a relatively common type.
+#' "Common types" are any file type that can be handled by the
+#' [readr](http://readr.tidyverse.org/), [readxl](http://readxl.tidyverse.org/),
+#' or [haven](http://haven.tidyverse.org/) packages.
+#'
+#' @param .data File path to read data from.
+#' @param tidyverse Should `readit` use functions available in the tidyverse,
+#'   e.g. functions from `readr`, etc.? Defaults to `TRUE`.
+# @param sheet If known to be an Excel file (`.xls`/`.xlsx`), this indicates
+#   the sheet to read. Defaults to the first sheet, and is passed on to
+#   [read_excel].
+#' @param ... Additional arguments passed to tidyverse read functions, e.g.
+#'   `sheet`, `n_max`, etc.
+#'
+#' @examples
+#' readit(system.file("examples", "csv.csv", package = "readit"))
+#' readit(system.file("examples", "tab_sep.txt", package = "readit"))
+#' readit(system.file("examples", "semi_sep.txt", package = "readit"))
+#' readit(system.file("examples", "xlsx.xlsx", package = "readit"))
+#' readit(system.file("examples", "xls.xls", package = "readit"))
+#' readit(system.file("examples", "iris.sas7bdat", package = "readit"))
+#'
+#' @export
+readit <- function(.data, ..., tidyverse = TRUE) {
+
+  dots <- list(...)
+  if ("delim" %in% names(dots))
+    stop(red$bold("If you're going to specify a delimiter, just use a specialized function!"))
+
+  ext <- tolower(tools::file_ext(.data))
+
+  if (tidyverse) {
+
+    if (ext == "txt") {
+      guess_txt(.data)
+    } else if (ext == "csv") {
+      .read_fun$read_guess <- "CSV"
+      .read_fun$read_fun <- function(x) read_csv(x, ...)
+    } else if (grepl("xls", ext)) {
+      .read_fun$read_guess <- "xls/xlsx (Excel)"
+      .read_fun$read_fun <- function(x) read_excel(x, ...)
+    } else if (grepl("^dta$|^sas7|^sav$|^por$", ext)) {
+      guess_haven(.data)
+    } else {
+      stop(red$bold("Unrecognized file extension, or file does not exist"))
+    }
+
+  } else {
+    stop(red$bold("Currently, only tidyverse functions are supported."))
+  }
+
+  message(green$bold(sprintf("File guessed to be %s (%s)",
+                             .read_fun$read_guess, deparse(substitute(.data)))))
+  .read_fun$read_fun(.data, ...)
+
+}
+
+
+
 #' Guess ".txt" Delimiter
 #'
 #' Since a `.txt` file is of ambiguous delimitation, this function is a helper
@@ -11,13 +76,21 @@ guess_txt <- function(.data, ...) {
 
   # Make sure then names are verbose, for sending console messages
   delims <- list(
-    "comma-delimited" = function(x) read_csv(x, ...),
-    "tab-delimited" = function(x) read_tsv(x, ...),
-    "semi-delimited" = function(x) read_csv2(x, ...),
-    "pipe-delimited" = function(x) read_delim(x, delim = "|", ...),
-    "space-delimited" = function(x) read_table2(x, ...))
+    "comma-delimited" = function(x) read_csv(x, dots),
+    "tab-delimited" = function(x) read_tsv(x, dots),
+    "semi-delimited" = function(x) read_csv2(x, dots),
+    "pipe-delimited" = function(x) read_delim(x, delim = "|", dots),
+    "space-delimited" = function(x) read_table2(x, dots))
 
-  best_delim <- lapply(delims, function(y)
+  n_max <- 100
+  delims_test <- list(
+    "comma-delimited" = function(x) read_csv(x, n_max = n_max),
+    "tab-delimited" = function(x) read_tsv(x, n_max = n_max),
+    "semi-delimited" = function(x) read_csv2(x, n_max = n_max),
+    "pipe-delimited" = function(x) read_delim(x, delim = "|", n_max = n_max),
+    "space-delimited" = function(x) read_table2(x, n_max = n_max))
+
+  best_delim <- lapply(delims_test, function(y)
     length(colnames(suppressMessages(suppressWarnings(y(.data))))))
   best_delim <- unlist(best_delim)
 
@@ -68,69 +141,4 @@ guess_haven <- function(.data, ...) {
     .read_fun$read_guess <- "POR (SPSS)"
     .read_fun$read_fun <- function(x) read_por(x, ...)
   }
-}
-
-
-
-#' Read Files of Any Type
-#'
-#' Given a file path, read the data into R, regardless of file type/extension.
-#' `readit` is a thick wrapper around many of the
-#' [tidyverse](https://www.tidyverse.org/) libraries, but can be forced to use
-#' base functions where possible. Note that the caveat is that the file
-#' _**needs**_ to have an extension, as well as be of a relatively common type.
-#' "Common types" are any file type that can be handled by the
-#' [readr](http://readr.tidyverse.org/), [readxl](http://readxl.tidyverse.org/),
-#' or [haven](http://haven.tidyverse.org/) packages.
-#'
-#' @param .data File path to read data from.
-#' @param tidyverse Should `readit` use functions available in the tidyverse,
-#'   e.g. functions from `readr`, etc.? Defaults to `TRUE`.
-# @param sheet If known to be an Excel file (`.xls`/`.xlsx`), this indicates
-#   the sheet to read. Defaults to the first sheet, and is passed on to
-#   [read_excel].
-#' @param ... Additional arguments passed to tidyverse read functions, e.g.
-#'   `sheet`, `n_max`, etc.
-#'
-#' @examples
-#' readit(system.file("examples", "csv.csv", package = "readit"))
-#' readit(system.file("examples", "tab_sep.txt", package = "readit"))
-#' readit(system.file("examples", "semi_sep.txt", package = "readit"))
-#' readit(system.file("examples", "xlsx.xlsx", package = "readit"))
-#' readit(system.file("examples", "xls.xls", package = "readit"))
-#' readit(system.file("examples", "iris.sas7bdat", package = "readit"))
-#'
-#' @export
-readit <- function(.data, tidyverse = TRUE, ...) {
-
-  dots <- list(...)
-  if ("delim" %in% names(dots))
-    stop(red$bold("If you're going to specify a delimiter, just use a specialized function!"))
-
-  ext <- tolower(tools::file_ext(.data))
-
-  if (tidyverse) {
-
-    if (ext == "txt") {
-      guess_txt(.data)
-    } else if (ext == "csv") {
-      .read_fun$read_guess <- "CSV"
-      .read_fun$read_fun <- function(x) read_csv(x, ...)
-    } else if (grepl("xls", ext)) {
-      .read_fun$read_guess <- "xls/xlsx (Excel)"
-      .read_fun$read_fun <- function(x) read_excel(x, ...)
-    } else if (grepl("^dta$|^sas7|^sav$|^por$", ext)) {
-      guess_haven(.data)
-    } else {
-      stop(red$bold("Unrecognized file extension, or file does not exist"))
-    }
-
-  } else {
-    stop(red$bold("Currently, only tidyverse functions are supported."))
-  }
-
-  message(green$bold(sprintf("File guessed to be %s (%s)",
-                             .read_fun$read_guess, deparse(substitute(.data)))))
-  .read_fun$read_fun(.data)
-
 }
